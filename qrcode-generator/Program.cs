25d089b4a145cb3d8039bcc2aec1304648bd04cf
Jorge Gaona <jgaona@akamai.com>
2025-03-19 19:33:38 -0600

    Add https config

--- qrcode-generator/Program.cs
+++ qrcode-generator/Program.cs
@@ -1,17 +1,30 @@
 using QRCoder;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
+using static QRCoder.QRCodeGenerator;
 
-
+// Configuring application
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.UseHttpsRedirection();
 
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = Status308PermanentRedirect;
+        options.HttpsPort = 8081;
+    });
+}
+
+// Routes
 app.MapGet("/", () => "QR Code Generator running as Knative Service");
 
 app.MapGet("/generate", async (HttpContext context) =>
 {
-    string? text = context.Request.Query["text"];
+    string? text = context.Request.Query["text"]; // Required
+    // Optional parameters
+    string? requestedVersion = context.Request.Query["version"];
+    string level = context.Request.Query["level"].First() ?? "M";
+
     if (string.IsNullOrEmpty(text))
     {
         context.Response.StatusCode = 400;
@@ -19,8 +32,43 @@ app.MapGet("/generate", async (HttpContext context) =>
         return;
     }
 
+
+    int version = -1;
+    int.TryParse(requestedVersion, out version);
+
+
+    ECCLevel eccLevel;
+    switch (level)
+    {
+
+        case "L":
+            eccLevel = ECCLevel.L;
+            break;
+        case "M":
+            eccLevel = ECCLevel.M;
+            break;
+        case "Q":
+            eccLevel = ECCLevel.Q;
+            break;
+        case "H":
+            eccLevel = ECCLevel.H;
+            break;
+        default:
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("Please L, M, Q, or H as the ECC Level");
+            return;
+    }
+
     using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
-    using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q))
+    using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(
+    text,
+    eccLevel,
+    forceUtf8: true,
+    utf8BOM: true,
+    eciMode: EciMode.Utf8,
+    requestedVersion: version))
+
+    //v6-M 2017
     using (PngByteQRCode qrCode = new PngByteQRCode(qrCodeData))
     {
         byte[] imageBytes = qrCode.GetGraphic(20);
